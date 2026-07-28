1519b783bd387c8b80494e713ae73d61027faa10
N£utr0nys <110538448+leoraclet@users.noreply.github.com>
2026-07-29 00:13:22 +0200

    direnv.nix: Added flake nixosModule & Moved app-image.nix to programs directory

---
+++ modules/system/programs/direnv.nix
@@ -0,0 +1,16 @@
+{
+  flake.nixosModules.direnv = { pkgs, ... }: {
+    # https://search.nixos.org/options?channel=26.05&query=programs.direnv
+    programs = {
+      direnv = {
+        enable = true;
+        silent = true;
+        loadInNixShell = true;
+        nix-direnv.enable = true;
+      };
+    };
+
+    # https://github.com/nix-community/lorri
+    services.lorri.enable = false;
+  };
+}
