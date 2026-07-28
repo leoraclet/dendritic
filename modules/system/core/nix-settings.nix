cac4430d75f6658900d8711fd1105c142b0c69f8
N£utr0nys <110538448+leoraclet@users.noreply.github.com>
2026-07-28 21:07:54 +0200

    Added power service and cachix infoss for Hyprland and Noctalia Shell

--- modules/system/core/nix-settings.nix
+++ modules/system/core/nix-settings.nix
@@ -1,5 +1,15 @@
 {
   flake.nixosModules.nix = { ... }: {
+    nixpkgs.config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+      allowBroken = true;
+    };
+
+    nixpkgs.config.permittedInsecurePackages = [
+      "openssl-1.1.1w"
+    ];
+
     documentation = {
       enable = true;
       doc.enable = false;
@@ -53,10 +63,20 @@
         substituters = [
           "https://cache.nixos.org"
           "https://nix-community.cachix.org/"
+          "https://hyprland.cachix.org"
+          "https://noctalia.cachix.org"
+        ];
+        trusted-substituters = [
+          "https://cache.nixos.org"
+          "https://nix-community.cachix.org/"
+          "https://hyprland.cachix.org"
+          "https://noctalia.cachix.org"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
         ];
 
         builders-use-substitutes = true;
--- modules/system/core/power.nix
+++ modules/system/core/power.nix
@@ -1,12 +1,13 @@
 {
   flake.nixosModules.power = { ... }: {
-    ervices = {
+    services = {
       upower.enable = true; # Power management daemon, required for battery and power management
       thermald.enable = true; # Thermal management daemon, useful for laptops to prevent overheating
       tlp.enable = true; # TLP is a power management tool for Linux, useful for laptops to extend battery life
 
       # ERROR: conflicts with services.tlp
       power-profiles-daemon.enable = false;
+      tuned.enable = false;
 
       # Enable Thunderbolt
       # https://nixos.wiki/wiki/Thunderbolt
