e4c96b23ffc1b5d7216446d6ac1f1f3d79b71e9b
N£utr0nys <110538448+leoraclet@users.noreply.github.com>
2026-07-28 23:52:26 +0200

    Added mount.nix

---
+++ modules/system/core/mount.nix
@@ -0,0 +1,9 @@
+{
+  flake.nixosModules.power = { pkgs, ... }: {
+    services = {
+      devmon.enable = true; # Automatically mount removable devices
+      udisks2.enable = true; # Disk management daemon, required for mounting and unmounting disks
+      gvfs.enable = true; # Mount, trash, and other functionalities
+    };
+  };
+}
